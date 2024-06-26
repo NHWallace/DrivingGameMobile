e7c677aefff25078c1040c3f605b510136c9ed34
NHWallace <54483659+NHWallace@users.noreply.github.com>
2024-06-25 18:34:26 -0700

    add energy system

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,15 +8,51 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text highScoreText;
+    [SerializeField] private TMP_Text energyText;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private float energyRechargeTime;
+
+    private int energy;
+
+    private const string EnergyKey = "Energy";
+    private const string EnergyReadyKey = "EnergyReady";
 
     private void Start()
     {
         int highScore = PlayerPrefs.GetInt(ScoreManager.HighScoreKey, 0);
         highScoreText.text = $"High Score: {highScore}";
+
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+
+        if(energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+
+            if (energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if ( DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            }
+        }
+
+        energyText.text = $"Play ({energy})";
     }
 
     public void Play()
     {
+        if (energy <= 0) { return; }
+
+        if (energy == maxEnergy)
+        {
+            PlayerPrefs.SetString(EnergyReadyKey, DateTime.Now.AddMinutes(energyRechargeTime).ToString());
+        }
+        energy--;
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
         SceneManager.LoadScene(1);
     }
 }
