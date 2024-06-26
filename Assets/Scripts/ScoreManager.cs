10cdbd19dbeb1bb35231fe1566797e2a29f6a72d
NHWallace <54483659+NHWallace@users.noreply.github.com>
2024-06-25 17:54:22 -0700

    add main menu, high score tracking

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,6 +7,8 @@ public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text scoreText;
 
+    public const string HighScoreKey = "HighScore";
+
     private float score;
 
     private void Update()
@@ -14,5 +16,14 @@ public class ScoreManager : MonoBehaviour
         score += Time.deltaTime * 5;
 
         scoreText.text = ((int)score).ToString();
+    }
+
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, (int)score);
+        }
     }
 }
